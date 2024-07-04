01a1a57947aa92ecb08872e18f6e8a185c32bbae
nirmalakoliya <nirmal.akoliya@engineering.gigakelvin.com>
2024-07-04 16:55:58 +0530

    update server file

--- config/server.js
+++ config/server.js
@@ -1,10 +1,18 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array("APP_KEYS"),
   },
   webhooks: {
-    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
+    populateRelations: env.bool("WEBHOOKS_POPULATE_RELATIONS", false),
+  },
+  server: {
+    // Adjust timeout settings as needed
+    timeout: {
+      server: 30000, // 30 seconds, adjust based on your needs
+      socket: 30000, // 30 seconds, adjust based on your needs
+      // You can increase these values further if necessary
+    },
   },
 });
